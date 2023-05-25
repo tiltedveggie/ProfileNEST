3ac15f89e8351ef0b12804a8aa7696f3c67e6b8a
Alan <tiltedveggie@outlook.kr>
2023-05-25 18:01:27 -0300

    FIX: Property 'profile' is missing

--- src/users/tests/mocks/users.mock.ts
+++ src/users/tests/mocks/users.mock.ts
@@ -1,3 +1,4 @@
+import { Profile } from '../../../../src/users/profile.entity';
 import { User } from '../../../../src/users/user.entity';
 
 export const usersMock: User[] = [
@@ -6,20 +7,23 @@ export const usersMock: User[] = [
 		username: 'hola1',
 		password: 'holaxd1',
 		createdAt: new Date(),
-		authStrategy: null
+		authStrategy: null,
+		profile: new Profile()
 	},
 	{
 		id: 2,
 		username: 'hola2',
 		password: 'holaxd2',
 		createdAt: new Date(),
-		authStrategy: null
+		authStrategy: null,
+		profile: new Profile()
 	},
 	{
 		id: 3,
 		username: 'hola3',
 		password: 'holaxd3',
 		createdAt: new Date(),
-		authStrategy: null
+		authStrategy: null,
+		profile: new Profile()
 	}
 ];
